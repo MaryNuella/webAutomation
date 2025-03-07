597a65b1231695fa7fa7d50808d059522ed8e71e
MaryNuella <110086278+MaryNuella@users.noreply.github.com>
2025-03-07 16:30:33 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
   video: true,
   e2e: {
-    baseUrl: 'https://project-amplio.netlify.app',
+    baseUrl: 'https://someDummyLinkThatDoesNotExist.netlify.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
